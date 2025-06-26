0f288c93e58edecd26fc2a9926bf287f1ed88f8b
Tyler Pollard <tylerpollard410@gmail.com>
2025-06-26 02:06:18 -0400

    Update data reading

--- inst/start_up_data.R
+++ inst/start_up_data.R
@@ -11,6 +11,8 @@ game_data_long  <- load_game_data_long(game_df = game_data)
 season_weeks_df <- game_data |> dplyr::distinct(season, week, week_seq)
 
 base_repo_url <- "https://github.com/TylerPollard410/nflendzoneData/releases/download/"
-season_standings_data <- readRDS(url(paste0(base_repo_url, "season_standings/season_standings.rds")))
-feature_long_data <- readRDS(url(paste0(base_repo_url, "feature_long/feature_long.rds")))
+# season_standings_data <- readRDS(url(paste0(base_repo_url, "season_standings/season_standings.rds")))
+# feature_long_data <- readRDS(url(paste0(base_repo_url, "feature_long/feature_long.rds")))
+season_standings_data <- nflreadr::rds_from_url(paste0(base_repo_url, "season_standings/season_standings.rds"))
+feature_long_data <- nflreadr::rds_from_url(paste0(base_repo_url, "feature_long/feature_long.rds"))
 # —– END STARTUP —–
